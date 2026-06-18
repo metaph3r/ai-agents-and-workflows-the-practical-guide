e9c5ebf3d49c3ee0112a1b0860923425416e1e6a
Silvio Glöckner <silvio.gloeckner@adesso.de>
2026-06-18 12:52:48 +0200

    prototype implementation

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+def generate_x_post(prompt: str) -> str:
+    # call AI / LLM
+    pass
+
 def main():
-    print("Hello from ai-agents-and-workflows-the-practical-guide!")
+    # user input => AI (LLM) to generate X posts => output post
+    usr_input = input("What should the post be about? ")
+    x_post = generate_x_post(usr_input)
+    print("Generated X post")
+    print (x_post)
 
 
 if __name__ == "__main__":
